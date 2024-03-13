fb7c4a019cc68e6a122457b4affbf04d5db35aa6
nito0415 <jsanc307@calpoly.edu>
2024-03-12 21:43:26 -0700

    code cleanup

--- project_db_mentors.sql
+++ project_db_mentors.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE IF NOT EXISTS project_db_mentors;
+
 DROP TABLE IF EXISTS onboarding_appt_mentees;
 DROP TABLE IF EXISTS onboarding_appt_mentors;
 DROP TABLE IF EXISTS onboarding_appt;
@@ -128,60 +130,60 @@ CREATE TABLE onboarding_appt_mentees (
 -- Not using real data for privacy reasons
 
 -- Company
-INSERT INTO company (company_id, company_name, company_sector, company_size, company_location, company_website, company_phone)
+INSERT INTO company (company_name, company_sector, company_size, company_location, company_website, company_phone)
 VALUES
-(1, 'Google', 'Technology', 100000, 'Mountain View, CA', 'https://www.google.com/', '650-253-0000'),
-(2, 'Facebook', 'Technology', 60000, 'Menlo Park, CA', 'https://www.facebook.com/', '650-543-4800'),
-(3, 'Amazon', 'Technology', 1000000, 'Seattle, WA', 'https://www.amazon.com/', '206-266-1000'),
-(4, 'Microsoft', 'Technology', 150000, 'Redmond, WA', 'https://www.microsoft.com/', '425-882-8080'),
-(5, 'Apple', 'Technology', 150000, 'Cupertino, CA', 'https://www.apple.com/', '408-996-1010'),
-(6, 'Tesla', 'Automotive', 48000, 'Palo Alto, CA', 'https://www.tesla.com/', '888-518-3752'),
-(7, 'Walmart', 'Retail', 2200000, 'Bentonville, AR', 'https://www.walmart.com/', '800-925-6278'),
-(8, 'Target', 'Retail', 360000, 'Minneapolis, MN', 'https://www.target.com/', '800-440-0680'),
-(9, 'Costco', 'Retail', 273000, 'Issaquah, WA', 'https://www.costco.com/', '800-774-2678'),
-(10, 'Starbucks', 'Food & Beverage', 346000, 'Seattle, WA', 'https://www.starbucks.com/', '800-782-7282');
+('Google', 'Technology', 100000, 'Mountain View, CA', 'https://www.google.com/', '650-253-0000'),
+('Facebook', 'Technology', 60000, 'Menlo Park, CA', 'https://www.facebook.com/', '650-543-4800'),
+('Amazon', 'Technology', 1000000, 'Seattle, WA', 'https://www.amazon.com/', '206-266-1000'),
+('Microsoft', 'Technology', 150000, 'Redmond, WA', 'https://www.microsoft.com/', '425-882-8080'),
+('Apple', 'Technology', 150000, 'Cupertino, CA', 'https://www.apple.com/', '408-996-1010'),
+('Tesla', 'Automotive', 48000, 'Palo Alto, CA', 'https://www.tesla.com/', '888-518-3752'),
+('Walmart', 'Retail', 2200000, 'Bentonville, AR', 'https://www.walmart.com/', '800-925-6278'),
+('Target', 'Retail', 360000, 'Minneapolis, MN', 'https://www.target.com/', '800-440-0680'),
+('Costco', 'Retail', 273000, 'Issaquah, WA', 'https://www.costco.com/', '800-774-2678'),
+('Starbucks', 'Food & Beverage', 346000, 'Seattle, WA', 'https://www.starbucks.com/', '800-782-7282');
 
 -- Mentors
-INSERT INTO mentors (mentor_id, first_name, last_name, alumni_type, years_of_experience, industry_sector, job_title, industry, email, phone, company_id)
+INSERT INTO mentors (first_name, last_name, alumni_type, years_of_experience, industry_sector, job_title, industry, email, phone, company_id)
 VALUES
-(1, 'John', 'Doe', 'Alumni', 10, 'Technology', 'Software Engineer', 'Technology', 'johndoe@example.com', '123-456-7890', 1),
-(2, 'Jane', 'Smith', 'Alumni', 15, 'Technology', 'Product Manager', 'Technology', 'janesmith@example.com', '123-456-7890', 2),
-(3, 'Michael', 'Johnson', 'Alumni', 20, 'Technology', 'Data Scientist', 'Technology', 'mikejohn@example.com', '123-456-7890', 3),
-(4, 'Emily', 'Williams', 'Alumni', 5, 'Technology', 'Software Engineer', 'Technology', 'emilywilliams@example.com', '123-456-7890', 4),
-(5, 'David', 'Brown', 'Alumni', 8, 'Technology', 'Product Manager', 'Technology', 'davidbrown@example.com', '123-456-7890', 5),
-(6, 'Olivia', 'Jones', 'Alumni', 12, 'Technology', 'Data Scientist', 'Technology', 'oliviajones@example.com', '123-456-7890', 6),
-(7, 'Daniel', 'Garcia', 'Alumni', 3, 'Technology', 'Software Engineer', 'Technology', 'dangarcia@example.com', '123-456-7890', 7),
-(8, 'Sophia', 'Martinez', 'Alumni', 7, 'Technology', 'Product Manager', 'Technology', 'sophiamartinez@example.com', '123-456-7890', 8),
-(9, 'Matthew', 'Rodriguez', 'Alumni', 9, 'Technology', 'Data Scientist', 'Technology', 'mettrodriguez@example.com', '123-456-7890', 9),
-(10, 'Isabella', 'Hernandez', 'Alumni', 11, 'Technology', 'Software Engineer', 'Technology', 'bellahernandez@example.com', '123-456-7890', 10);
+('John', 'Doe', 'Alumni', 10, 'Technology', 'Software Engineer', 'Technology', 'johndoe@example.com', '123-456-7890', 1),
+('Jane', 'Smith', 'Alumni', 15, 'Technology', 'Product Manager', 'Technology', 'janesmith@example.com', '123-456-7890', 2),
+('Michael', 'Johnson', 'Alumni', 20, 'Technology', 'Data Scientist', 'Technology', 'mikejohn@example.com', '123-456-7890', 3),
+('Emily', 'Williams', 'Alumni', 5, 'Technology', 'Software Engineer', 'Technology', 'emilywilliams@example.com', '123-456-7890', 4),
+('David', 'Brown', 'Alumni', 8, 'Technology', 'Product Manager', 'Technology', 'davidbrown@example.com', '123-456-7890', 5),
+('Olivia', 'Jones', 'Alumni', 12, 'Technology', 'Data Scientist', 'Technology', 'oliviajones@example.com', '123-456-7890', 6),
+('Daniel', 'Garcia', 'Alumni', 3, 'Technology', 'Software Engineer', 'Technology', 'dangarcia@example.com', '123-456-7890', 7),
+('Sophia', 'Martinez', 'Alumni', 7, 'Technology', 'Product Manager', 'Technology', 'sophiamartinez@example.com', '123-456-7890', 8),
+('Matthew', 'Rodriguez', 'Alumni', 9, 'Technology', 'Data Scientist', 'Technology', 'mettrodriguez@example.com', '123-456-7890', 9),
+('Isabella', 'Hernandez', 'Alumni', 11, 'Technology', 'Software Engineer', 'Technology', 'bellahernandez@example.com', '123-456-7890', 10);
 
 -- Mentees
-INSERT INTO mentees (mentee_id, major, grad_year, career_interests, first_name, last_name, email, phone, mentor_id)
+INSERT INTO mentees (major, grad_year, career_interests, first_name, last_name, email, phone, mentor_id)
 VALUES
-(1, 'Computer Science', 2024, 'Software Engineering', 'Jack', 'Black', 'jackblack@example.com', '123-456-7890', 1),
-(2, 'Computer Science', 2025, 'Product Management', 'Sam', 'Smith', 'samsmith@example.com', '123-456-7890', 2),
-(3, 'Computer Science', 2026, 'Data Science', 'Michael', 'Johnson', 'michael@example.com', '123-456-7890', 3),
-(4, 'Computer Science', 2025, 'Software Engineering', 'Camille', 'Jones', 'camille@example.com', '123-456-7890', 4),
-(5, 'Computer Science', 2026, 'Product Management', 'Keaton', 'Brown', 'keaton@example.com', '123-456-7890', 5),
-(6, 'Computer Science', 2024, 'Data Science', 'Jared', 'Garcia', 'jared@example.com', '123-456-7890', 6),
-(7, 'Computer Science', 2026, 'Software Engineering', 'Eliana', 'Martinez', 'eli@example.com', '123-456-7890', 7),
-(8, 'Computer Science', 2025, 'Product Management', 'Ava', 'Rodriguez', 'ava@example.com', '123-456-7890', 8),
-(9, 'Computer Science', 2026, 'Data Science', 'Ethan', 'Hernandez', 'ethan@example.com', '123-456-7890', 9),
-(10, 'Computer Science', 2024, 'Software Engineering', 'Avery', 'Smith', 'avery@example.com', '123-456-7890', 10);
+('Computer Science', 2024, 'Software Engineering', 'Jack', 'Black', 'jackblack@example.com', '123-456-7890', 1),
+('Computer Science', 2025, 'Product Management', 'Sam', 'Smith', 'samsmith@example.com', '123-456-7890', 2),
+('Computer Science', 2026, 'Data Science', 'Michael', 'Johnson', 'michael@example.com', '123-456-7890', 3),
+('Computer Science', 2025, 'Software Engineering', 'Camille', 'Jones', 'camille@example.com', '123-456-7890', 4),
+('Computer Science', 2026, 'Product Management', 'Keaton', 'Brown', 'keaton@example.com', '123-456-7890', 5),
+('Computer Science', 2024, 'Data Science', 'Jared', 'Garcia', 'jared@example.com', '123-456-7890', 6),
+('Computer Science', 2026, 'Software Engineering', 'Eliana', 'Martinez', 'eli@example.com', '123-456-7890', 7),
+('Computer Science', 2025, 'Product Management', 'Ava', 'Rodriguez', 'ava@example.com', '123-456-7890', 8),
+('Computer Science', 2026, 'Data Science', 'Ethan', 'Hernandez', 'ethan@example.com', '123-456-7890', 9),
+('Computer Science', 2024, 'Software Engineering', 'Avery', 'Smith', 'avery@example.com', '123-456-7890', 10);
 
 -- Mentoring Session
-INSERT INTO mentoring_session (session_id, session_date, session_time, session_duration, session_location, session_modality, session_topic)
+INSERT INTO mentoring_session (session_date, session_time, session_duration, session_location, session_modality, session_topic)
 VALUES
-(1, '2022-01-01', '10:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering'),
-(2, '2022-01-02', '11:00:00', 60, 'Zoom', 'Virtual', 'Product Management'),
-(3, '2022-01-03', '12:00:00', 60, 'Zoom', 'Virtual', 'Data Science'),
-(4, '2022-01-04', '13:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering'),
-(5, '2022-01-05', '14:00:00', 60, 'Zoom', 'Virtual', 'Product Management'),
-(6, '2022-01-06', '15:00:00', 60, 'Zoom', 'Virtual', 'Data Science'),
-(7, '2022-01-07', '16:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering'),
-(8, '2022-01-08', '17:00:00', 60, 'Zoom', 'Virtual', 'Product Management'),
-(9, '2022-01-09', '18:00:00', 60, 'Zoom', 'Virtual', 'Data Science'),
-(10, '2022-01-10', '19:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering');
+('2022-01-01', '10:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering'),
+('2022-01-02', '11:00:00', 60, 'Zoom', 'Virtual', 'Product Management'),
+('2022-01-03', '12:00:00', 60, 'Zoom', 'Virtual', 'Data Science'),
+('2022-01-04', '13:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering'),
+('2022-01-05', '14:00:00', 60, 'Zoom', 'Virtual', 'Product Management'),
+('2022-01-06', '15:00:00', 60, 'Zoom', 'Virtual', 'Data Science'),
+('2022-01-07', '16:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering'),
+('2022-01-08', '17:00:00', 60, 'Zoom', 'Virtual', 'Product Management'),
+('2022-01-09', '18:00:00', 60, 'Zoom', 'Virtual', 'Data Science'),
+('2022-01-10', '19:00:00', 60, 'Zoom', 'Virtual', 'Software Engineering');
 
 -- Mentors Sessions
 INSERT INTO mentors_sessions (mentor_id, session_id)
@@ -212,32 +214,32 @@ VALUES
 (10, 10);
 
 -- Career Peer Staff
-INSERT INTO career_peer_staff (staff_id, staff_role, first_name, last_name, email, phone)
+INSERT INTO career_peer_staff (staff_role, first_name, last_name, email, phone)
 VALUES
-(1, 'Career Peer', 'Aubrey', 'Graham', 'drake@example.com', '123-456-7890'),
-(2, 'Career Peer', 'Rihanna', 'Fenty', 'riri@example.com', '123-456-7890'),
-(3, 'Career Peer', 'Beyonce', 'Knowles', 'queenbey@example.com', '123-456-7890'),
-(4, 'Career Peer', 'Jeffrey', 'Free', 'freejeffrey@example.com', '123-456-7890'),
-(5, 'Career Peer', 'Kendrick', 'Lamar', 'kdot@example.com', '123-456-7890'),
-(6, 'Career Peer', 'Jack', 'Johnson', 'jj@example.com', '123-456-7890'),
-(7, 'Career Peer', 'John', 'Legend', 'jl@example.com', '123-456-7890'),
-(8, 'Career Peer', 'Alicia', 'Keys', 'alicia@example.com', '123-456-7890'),
-(9, 'Career Peer', 'Adele', 'Adkins', 'aa@example.com', '123-456-7890'),
-(10, 'Career Peer', 'Bruno', 'Mars', 'bm@example.com', '123-456-7890');
+('Career Peer', 'Aubrey', 'Graham', 'drake@example.com', '123-456-7890'),
+('Career Peer', 'Rihanna', 'Fenty', 'riri@example.com', '123-456-7890'),
+('Career Peer', 'Beyonce', 'Knowles', 'queenbey@example.com', '123-456-7890'),
+('Career Peer', 'Jeffrey', 'Free', 'freejeffrey@example.com', '123-456-7890'),
+('Career Peer', 'Kendrick', 'Lamar', 'kdot@example.com', '123-456-7890'),
+('Career Peer', 'Jack', 'Johnson', 'jj@example.com', '123-456-7890'),
+('Career Peer', 'John', 'Legend', 'jl@example.com', '123-456-7890'),
+('Career Peer', 'Alicia', 'Keys', 'alicia@example.com', '123-456-7890'),
+('Career Peer', 'Adele', 'Adkins', 'aa@example.com', '123-456-7890'),
+('Career Peer', 'Bruno', 'Mars', 'bm@example.com', '123-456-7890');
 
 -- Onboarding Appt
-INSERT INTO onboarding_appt (onboarding_appt_id, date, time, duration, appt_location, appt_modality, reason_for_appt, topics_covered, feedback, staff_id)
+INSERT INTO onboarding_appt (date, time, duration, appt_location, appt_modality, reason_for_appt, topics_covered, feedback, staff_id)
 VALUES
-(1, '2022-01-01', '10:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 1),
-(2, '2022-01-02', '11:00:00', 60, 'Zoom', 'Virtual', 'Interview Prep', 'Behavioral Interview, Technical Interview', 'Great session!', 2),
-(3, '2022-01-03', '12:00:00', 60, 'Zoom', 'Virtual', 'Networking', 'Elevator Pitch, Informational Interview', 'Great session!', 3),
-(4, '2022-01-04', '13:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 4),
-(5, '2022-01-05', '14:00:00', 60, 'Zoom', 'Virtual', 'Interview Prep', 'Behavioral Interview, Technical Interview', 'Great session!', 5),
-(6, '2022-01-06', '15:00:00', 60, 'Zoom', 'Virtual', 'Networking', 'Elevator Pitch, Informational Interview', 'Great session!', 6),
-(7, '2022-01-07', '16:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 7),
-(8, '2022-01-08', '17:00:00', 60, 'Zoom', 'Virtual', 'Interview Prep', 'Behavioral Interview, Technical Interview', 'Great session!', 8),
-(9, '2022-01-09', '18:00:00', 60, 'Zoom', 'Virtual', 'Networking', 'Elevator Pitch, Informational Interview', 'Great session!', 9),
-(10, '2022-01-10', '19:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 10);
+('2022-01-01', '10:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 1),
+('2022-01-02', '11:00:00', 60, 'Zoom', 'Virtual', 'Interview Prep', 'Behavioral Interview, Technical Interview', 'Great session!', 2),
+('2022-01-03', '12:00:00', 60, 'Zoom', 'Virtual', 'Networking', 'Elevator Pitch, Informational Interview', 'Great session!', 3),
+('2022-01-04', '13:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 4),
+('2022-01-05', '14:00:00', 60, 'Zoom', 'Virtual', 'Interview Prep', 'Behavioral Interview, Technical Interview', 'Great session!', 5),
+('2022-01-06', '15:00:00', 60, 'Zoom', 'Virtual', 'Networking', 'Elevator Pitch, Informational Interview', 'Great session!', 6),
+('2022-01-07', '16:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 7),
+('2022-01-08', '17:00:00', 60, 'Zoom', 'Virtual', 'Interview Prep', 'Behavioral Interview, Technical Interview', 'Great session!', 8),
+('2022-01-09', '18:00:00', 60, 'Zoom', 'Virtual', 'Networking', 'Elevator Pitch, Informational Interview', 'Great session!', 9),
+('2022-01-10', '19:00:00', 60, 'Zoom', 'Virtual', 'Resume Review', 'Resume, Cover Letter, LinkedIn Profile', 'Great session!', 10);
 
 -- Onboarding Appt Mentors
 INSERT INTO onboarding_appt_mentors (onboarding_appt_id, mentor_id)
@@ -267,102 +269,35 @@ VALUES
 (9, 9),
 (10, 10);
 
--- Statememts used to test AUTO_INCREMENT and FOREIGN KEY constraints
-
-INSERT INTO company (company_name, company_sector, company_size, company_location, company_website, company_phone)
-VALUES ('Pepsi Co.', 'Food & Beverage', 40000000, 'Harrison, NY', 'https://www.pepsi.com/', '800-123-4567');
-
-INSERT INTO mentees (major, grad_year, career_interests, first_name, last_name, email, phone, mentor_id)
-VALUES ('Economics' , '2024', 'Information Systems', 'Juan', 'Sanchez', 'jsanc@example.com', '800-123-4567', 1);
-
-INSERT INTO mentors (first_name, last_name, alumni_type, years_of_experience, industry_sector, job_title, industry, email, phone, company_id)
-VALUES ('Jack', 'Doherty', 'Alumni', 5, 'Technology', 'Software Engineer', 'Information Systems', 'jd@example.com', '800-123-4567', 11);
-
-INSERT INTO mentors_sessions (mentor_id, session_id)
-VALUES (11, 1);
-
-INSERT INTO mentees_sessions (mentee_id, session_id)
-VALUES (11, 1);
-
-SELECT * FROM company;
-
-
+-- Queries
 
 -- TODO: Create 3 queries that include grouping
-SELECT 
-    'Mentee' AS attendee_type,
-    m.first_name,
-    m.last_name,
-    m.email,
-    ms.session_id
-FROM 
-    mentees_sessions AS ms
-INNER JOIN 
-    mentees AS m ON ms.mentee_id = m.mentee_id
--- WHERE 
---    ms.session_id = 1
 
-UNION ALL
-
-SELECT 
-    'Mentor' AS attendee_type,
-    mt.first_name,
-    mt.last_name,
-    mt.email,
-    ms.session_id
-FROM 
-    mentors_sessions AS ms
-INNER JOIN 
-    mentors AS mt ON ms.mentor_id = mt.mentor_id
--- WHERE 
---    ms.session_id = 1
-
-ORDER BY 
-    session_id, attendee_type;
+-- View mentor and mentee assignment along with their names, email, and relevant career information
+CREATE VIEW mentee_mentor_info_session_1 AS
+SELECT 'Mentee' AS attendee_type, m.first_name, m.last_name, m.email, m.career_interests
+FROM mentees_sessions AS ms
+INNER JOIN mentees AS m ON ms.mentee_id = m.mentee_id
+WHERE ms.session_id = 1
+UNION 
+SELECT 'Mentor' AS attendee_type, mt.first_name, mt.last_name, mt.email, mt.industry
+FROM mentors_sessions AS ms
+INNER JOIN mentors AS mt ON ms.mentor_id = mt.mentor_id
+WHERE ms.session_id = 1;
 
 -- View mentoring sessions and their attendees
 
-SELECT 
-    s.session_id,
-    s.session_date,
-    s.session_time,
-    s.session_duration,
-    s.session_location,
-    s.session_topic,
-    s.session_modality,
-    'Mentee' AS attendee_type,
-    m.first_name,
-    m.last_name,
-    m.email
-FROM
-    mentoring_session AS s
-INNER JOIN
-    mentees_sessions AS ms ON s.session_id = ms.session_id
-INNER JOIN
-    mentees AS m ON ms.mentee_id = m.mentee_id
-
+SELECT s.session_id,s.session_date, s.session_time, s.session_duration, s.session_location, s.session_topic, s.session_modality, 
+    'Mentee' AS attendee_type, m.first_name, m.last_name, m.email
+FROM mentoring_session s
+INNER JOIN mentees_sessions AS ms ON s.session_id = ms.session_id
+INNER JOIN mentees m ON ms.mentee_id = m.mentee_id
 UNION ALL
-
-SELECT 
-    s.session_id,
-    s.session_date,
-    s.session_time,
-    s.session_duration,
-    s.session_location,
-    s.session_topic,
-    s.session_modality,
-    'Mentor' AS attendee_type,
-    mt.first_name,
-    mt.last_name,
-    mt.email
-FROM
-    mentoring_session AS s 
-INNER JOIN
-    mentors_sessions AS ms ON s.session_id = ms.session_id
-INNER JOIN
-    mentors AS mt ON ms.mentor_id = mt.mentor_id
-
-ORDER BY
-    session_id, attendee_type;
+SELECT s.session_id, s.session_date, s.session_time, s.session_duration, s.session_location, s.session_topic, s.session_modality,
+    'Mentor' AS attendee_type, mt.first_name, mt.last_name, mt.email
+FROM mentoring_session s 
+INNER JOIN mentors_sessions AS ms ON s.session_id = ms.session_id
+INNER JOIN mentors mt ON ms.mentor_id = mt.mentor_id
+ORDER BY session_id, attendee_type;
 
 -- TODO: Add 2 more queries that implement subqueries
